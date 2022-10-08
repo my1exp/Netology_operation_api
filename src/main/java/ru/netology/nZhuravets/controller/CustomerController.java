b41e11113689fe49961aa8fb66ef2f90a0ac63c6
Nikita Zhuravets <impozernovus@gmail.com>
2022-10-08 21:22:54 +0300

    asd

--- src/main/java/ru/netology/nZhuravets/controller/CustomerController.java
+++ src/main/java/ru/netology/nZhuravets/controller/CustomerController.java
@@ -1,13 +1,11 @@
 package ru.netology.nZhuravets.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.netology.nZhuravets.controller.dto.CustomersDTO;
 import ru.netology.nZhuravets.controller.dto.GetCustomersResponse;
 import ru.netology.nZhuravets.domain.Customer;
 import ru.netology.nZhuravets.service.CustomerService;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,16 +16,32 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
+
+    @GetMapping
     public GetCustomersResponse getCustomers(){
-        List<Customer> customers = customerService.getCustomers;
+        List<Customer> customers = customerService.getCustomers();
         List<CustomersDTO> customerDTOS = new ArrayList<>();
         for (Customer customer: customers) {
-            CustomersDTO customerDTO = new CustomersDTO();
-            customerDTO.setId(customer.getId());
-            customerDTO.setName(customer.getName());
+            CustomersDTO customerDTO = new CustomersDTO(customer.getId(), customer.getName());
             customerDTOS.add(customerDTO);
         }
         return new GetCustomersResponse(customerDTOS);
     }
-}
 
+
+    @GetMapping("/{customerId}")
+    public CustomersDTO getCustomer(@PathVariable int customerId){
+        for (Customer customer: customerService.getCustomers()) {
+            if (customer.getId() == customerId){
+                CustomersDTO customersDTO = new CustomersDTO(customer.getId(), customer.getName());
+                return customersDTO;
+            }
+        }
+        return null;
+    }
+
+    @PostMapping("/{customerId}")
+    public void addCustomer(@RequestBody  Customer customer){
+        customerService.addCustomer(customer.getId(), customer.getName());
+    }
+}
